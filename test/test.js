c1ed63f3caf0094d582d4b882cd068db75fcc100
Rin Minase <rin.black.raison.detre@gmail.com>
2020-04-20 17:13:12 +0800

    Added template test script

---
+++ test/test.js
@@ -0,0 +1,9 @@
+const { assert, expect, should } = require('chai');
+
+describe('Array', () => {
+	describe('#indexOf()', () => {
+		it('should return -1 when the value is not present', () => {
+			assert.equal([1, 2, 3].indexOf(4), -1);
+		});
+	});
+});
