f178f52d54efc6e80190003290b5504f856267ae
Rin Minase <rin.black.raison.detre@gmail.com>
2020-04-23 05:44:36 +0800

    Added testing for undo using objects

--- test.js
+++ test.js
@@ -1,9 +1,42 @@
 const { assert, expect, should } = require('chai');
+const historyHandler = require("./dist/history-handler").default;
 
-describe('Array', () => {
-	describe('#indexOf()', () => {
-		it('should return -1 when the value is not present', () => {
-			assert.equal([1, 2, 3].indexOf(4), -1);
+describe('undo()', () => {
+	it('should return invalid when undo buffer is empty', () => {
+		const history = historyHandler();
+		const invalid = history.undo().invalid;
+
+		assert.equal(invalid, true);
+	});
+
+	describe('Objects', () => {
+		it('should return the top stack of the undo buffer', () => {
+			const history = historyHandler();
+	
+			history.action({value: 1});
+			/**
+			 * Buffers should now be:
+			 *   Undo: [],
+			 *   Buffer: {value: 1}
+			 */
+
+			 history.action({value: 2});
+			/**
+			 * Buffers should now be:
+			 *   Undo: [{value: 1}],
+			 *   Buffer: {value: 2}
+			 */
+
+			history.action({value: 3});
+			/**
+			 * Buffers should now be:
+			 *   Undo: [{value: 1}, {value: 2}],
+			 *   Buffer: {value: 3}
+			 */
+	
+			const value = history.undo().value;
+	
+			assert.equal(value, 2);
 		});
 	});
 });
