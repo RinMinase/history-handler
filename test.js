4d37bf1ffc08a82a2e8a9bee8d7129943ee26d2c
Rin Minase <rin.black.raison.detre@gmail.com>
2020-04-23 05:51:18 +0800

    Modularized testing files

--- test.js
+++ test.js
@@ -1,42 +1,15 @@
 const { assert, expect, should } = require('chai');
-const historyHandler = require("./dist/history-handler").default;
+const historyHandler = require('./dist/history-handler').default;
 
-describe('undo()', () => {
-	it('should return invalid when undo buffer is empty', () => {
-		const history = historyHandler();
-		const invalid = history.undo().invalid;
-
-		assert.equal(invalid, true);
-	});
-
-	describe('Objects', () => {
-		it('should return the top stack of the undo buffer', () => {
+describe('HistoryHandler', () => {
+	describe('#undo()', () => {
+		it('should return invalid when undo buffer is empty', () => {
 			const history = historyHandler();
-	
-			history.action({value: 1});
-			/**
-			 * Buffers should now be:
-			 *   Undo: [],
-			 *   Buffer: {value: 1}
-			 */
+			const invalid = history.undo().invalid;
 
-			 history.action({value: 2});
-			/**
-			 * Buffers should now be:
-			 *   Undo: [{value: 1}],
-			 *   Buffer: {value: 2}
-			 */
-
-			history.action({value: 3});
-			/**
-			 * Buffers should now be:
-			 *   Undo: [{value: 1}, {value: 2}],
-			 *   Buffer: {value: 3}
-			 */
-	
-			const value = history.undo().value;
-	
-			assert.equal(value, 2);
+			assert.equal(invalid, true);
 		});
+
+		require('./tests/undo-objects');
 	});
-});
+})
