9738ae1292176aafecf72bb8fc3946aab3b6918d
Rin Minase <rin.black.raison.detre@gmail.com>
2020-04-23 20:12:07 +0800

    Modified to use tabs instead of spaces

--- index.js
+++ index.js
@@ -1,95 +1,95 @@
 export default function history() {
-    let currentBuffer = null;
-    let undoBuffer = [];
-    let redoBuffer = [];
+	let currentBuffer = null;
+	let undoBuffer = [];
+	let redoBuffer = [];
 
-    return {
+	return {
 
-        /**
-         * 
-         */
-        clear: function() {
-            currentBuffer = null;
-            undoBuffer = [];
-            redoBuffer = [];
-        },
+		/**
+		 * 
+		 */
+		clear: function() {
+			currentBuffer = null;
+			undoBuffer = [];
+			redoBuffer = [];
+		},
 
-        /**
-         * 
-         */
-        getBuffer: function() {
-            if (currentBuffer) {
-                return currentBuffer;
-            }
-        },
-        
-        /**
-         * 
-         */
-        getUndoBuffer: function() {
-            return undoBuffer;
-        },
-        
-        /**
-         * 
-         */
-        getRedoBuffer: function() {
-            return redoBuffer;
-        },
+		/**
+		 * 
+		 */
+		getBuffer: function() {
+			if (currentBuffer) {
+				return currentBuffer;
+			}
+		},
+		
+		/**
+		 * 
+		 */
+		getUndoBuffer: function() {
+			return undoBuffer;
+		},
+		
+		/**
+		 * 
+		 */
+		getRedoBuffer: function() {
+			return redoBuffer;
+		},
 
-        /**
-         * 
-         * @param {object | number |string} obj 
-         */
-        action: function(obj) {
-            if (currentBuffer) {
-                undoBuffer.push(Object.assign({}, currentBuffer));
+		/**
+		 * 
+		 * @param {object | number |string} obj 
+		 */
+		action: function(obj) {
+			if (currentBuffer) {
+				undoBuffer.push(Object.assign({}, currentBuffer));
 
-                if (undoBuffer.length > 10) {
-                    undoBuffer.splice(1, 1);
-                }
-            }
+				if (undoBuffer.length > 10) {
+					undoBuffer.splice(1, 1);
+				}
+			}
 
-            currentBuffer = obj;
-            redoBuffer = [];
-        },
+			currentBuffer = obj;
+			redoBuffer = [];
+		},
 
-        /**
-         * 
-         */
-        undo: function() {
-            if (undoBuffer.length) {
-                const buffer = undoBuffer.pop();
-                redoBuffer.push(Object.assign({}, currentBuffer));
-                currentBuffer = buffer;
+		/**
+		 * 
+		 */
+		undo: function() {
+			if (undoBuffer.length) {
+				const buffer = undoBuffer.pop();
+				redoBuffer.push(Object.assign({}, currentBuffer));
+				currentBuffer = buffer;
 
-                if (redoBuffer.length > 10) {
-                    redoBuffer.splice(1, 1);
-                }
+				if (redoBuffer.length > 10) {
+					redoBuffer.splice(1, 1);
+				}
 
-                return buffer;
-            }
+				return buffer;
+			}
 
-            return { invalid: true }
-        },
+			return { invalid: true }
+		},
 
-        /**
-         * 
-         */
-        redo: function() {
-            if (redoBuffer.length) {
-                const buffer = redoBuffer.pop();
-                undoBuffer.push(Object.assign({}, currentBuffer));
-                currentBuffer = buffer;
+		/**
+		 * 
+		 */
+		redo: function() {
+			if (redoBuffer.length) {
+				const buffer = redoBuffer.pop();
+				undoBuffer.push(Object.assign({}, currentBuffer));
+				currentBuffer = buffer;
 
-                if (undoBuffer.length > 10) {
-                    undoBuffer.splice(1, 1);
-                }
+				if (undoBuffer.length > 10) {
+					undoBuffer.splice(1, 1);
+				}
 
-                return buffer;
-            }
+				return buffer;
+			}
 
-            return { invalid: true }
-        }
-    }
+			return { invalid: true }
+		}
+	}
 }
