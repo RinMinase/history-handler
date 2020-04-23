5787c4f6a943a02a0574e0ca6310536aa5446a96
Rin Minase <rin.black.raison.detre@gmail.com>
2020-04-23 20:12:23 +0800

    Modified test scripts to use tabs instead of spaces

--- tests/undo-objects.js
+++ tests/undo-objects.js
@@ -2,32 +2,32 @@ const { assert, expect, should } = require('chai');
 const historyHandler = require('../dist/history-handler').default;
 
 describe('Objects', () => {
-    it('should return the top stack of the undo buffer', () => {
-        const history = historyHandler();
+	it('should return the top stack of the undo buffer', () => {
+		const history = historyHandler();
 
-        history.action({value: 1});
-        /**
-         * Buffers should now be:
-         *   Undo: [],
-         *   Buffer: {value: 1}
-         */
+		history.action({value: 1});
+		/**
+		 * Buffers should now be:
+		 *   Undo: [],
+		 *   Buffer: {value: 1}
+		 */
 
-         history.action({value: 2});
-        /**
-         * Buffers should now be:
-         *   Undo: [{value: 1}],
-         *   Buffer: {value: 2}
-         */
+		 history.action({value: 2});
+		/**
+		 * Buffers should now be:
+		 *   Undo: [{value: 1}],
+		 *   Buffer: {value: 2}
+		 */
 
-        history.action({value: 3});
-        /**
-         * Buffers should now be:
-         *   Undo: [{value: 1}, {value: 2}],
-         *   Buffer: {value: 3}
-         */
+		history.action({value: 3});
+		/**
+		 * Buffers should now be:
+		 *   Undo: [{value: 1}, {value: 2}],
+		 *   Buffer: {value: 3}
+		 */
 
-        const value = history.undo().value;
+		const value = history.undo().value;
 
-        assert.equal(value, 2);
-    });
+		assert.equal(value, 2);
+	});
 });
