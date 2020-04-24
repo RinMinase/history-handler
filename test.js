4a6eeec1fbd4f8a6361f59fefd550763c3874ad9
Rin Minase <rin.black.raison.detre@gmail.com>
2020-04-24 17:02:47 +0800

    Moved tests for constructor

--- test.js
+++ test.js
@@ -3,41 +3,7 @@ const historyHandler = require('./dist/history-handler').default;
 
 describe('HistoryHandler', () => {
 	describe('__constructor(length)', () => {
-		it('should increase the buffer size', () => {
-			const history = historyHandler(15);
-
-			history.action({value: 1});
-			history.action({value: 2});
-			history.action({value: 3});
-			history.action({value: 4});
-			history.action({value: 5});
-			history.action({value: 6});
-			history.action({value: 7});
-			history.action({value: 8});
-			history.action({value: 9});
-			history.action({value: 10});
-			history.action({value: 11});
-			/**
-			 * Buffers should now be:
-			 *   Undo: [{value: 1}...{value: 10}]
-			 *   Buffer: {value: 11}
-			 */
-
-			history.action({value: 12});
-			history.action({value: 13});
-			history.action({value: 14});
-			history.action({value: 15});
-			history.action({value: 16});
-			/**
-			 * Buffers should now be:
-			 *   Undo: [{value: 1}...{value: 15}]
-			 *   Buffer: {value: 16}
-			 */
-
-			const buffer = history.getUndoBuffer();
-
-			assert.deepEqual(buffer[0], {value: 1});
-		});
+		require('./tests/constructor');
 	});
 
 	describe('#clear()', () => {
